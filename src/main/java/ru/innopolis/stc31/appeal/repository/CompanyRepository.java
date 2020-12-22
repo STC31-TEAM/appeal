9e877ad7218a1a39e8462abcffc6226dd2127ccb
Mikhail Kutcin <makq3345@gmail.com>
2020-12-22 16:42:08 +0500

    - добавлены интерфейсы репозиториев

--- src/main/java/ru/innopolis/stc31/appeal/repository/CompanyRepository.java
+++ src/main/java/ru/innopolis/stc31/appeal/repository/CompanyRepository.java
@@ -2,8 +2,8 @@ package ru.innopolis.stc31.appeal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.innopolis.stc31.appeal.model.ModelMock;
+import ru.innopolis.stc31.appeal.model.entity.Company;
 
 @Repository
-public interface TicketRepository extends JpaRepository<ModelMock, Long> {
+public interface CompanyRepository  extends JpaRepository<Company, Integer> {
 }
--- src/main/java/ru/innopolis/stc31/appeal/repository/TicketsUsersRepository.java
+++ src/main/java/ru/innopolis/stc31/appeal/repository/TicketsUsersRepository.java
@@ -2,8 +2,8 @@ package ru.innopolis.stc31.appeal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.innopolis.stc31.appeal.model.ModelMock;
+import ru.innopolis.stc31.appeal.model.entity.TicketsUsers;
 
 @Repository
-public interface TicketRepository extends JpaRepository<ModelMock, Long> {
+public interface TicketsUsersRepository extends JpaRepository<TicketsUsers, Integer> {
 }
