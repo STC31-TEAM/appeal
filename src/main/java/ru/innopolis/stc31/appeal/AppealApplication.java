3b7f52abe0779d90a29f9ac9a6c0c97de711cdcd
SFomin <sergei.fomin@list.ru>
2021-01-05 23:05:00 +0500

    removed excess extend

--- src/main/java/ru/innopolis/stc31/appeal/AppealApplication.java
+++ src/main/java/ru/innopolis/stc31/appeal/AppealApplication.java
@@ -3,13 +3,13 @@ package ru.innopolis.stc31.appeal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 /**
  * Application entry point
  */
 @SpringBootApplication
 @EntityScan(basePackages= {"ru.innopolis.stc31.appeal.model"})
-public class AppealApplication extends SpringBootServletInitializer {
+public class AppealApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppealApplication.class, args);
