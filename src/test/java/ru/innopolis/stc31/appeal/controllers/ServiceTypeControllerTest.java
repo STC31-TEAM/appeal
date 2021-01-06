abda01c7b1bdabc3f4dbe7e29e28bc4ad6df5699
SFomin <sergei.fomin@list.ru>
2021-01-06 22:45:50 +0500

    trying exclude coverage models

--- src/test/java/ru/innopolis/stc31/appeal/controllers/ServiceTypeControllerTest.java
+++ src/test/java/ru/innopolis/stc31/appeal/controllers/ServiceTypeControllerTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 /**
- * Testing RoleController
+ * Testing ServiceTypeController
  */
 @SpringJUnitConfig
 class ServiceTypeControllerTest {
