ea8cec87e48956249feb4c8643279e98bc635735
Mikhail Kutcin <makq3345@gmail.com>
2021-01-06 09:08:26 +0500

    Добавлены автотесты для конверторов Country, Street. UserControllerTest замена на @SpringBootTest и @ActiveProfiles({"test"})

--- src/test/java/ru/innopolis/stc31/appeal/controllers/UserControllerTest.java
+++ src/test/java/ru/innopolis/stc31/appeal/controllers/UserControllerTest.java
@@ -3,6 +3,8 @@ package ru.innopolis.stc31.appeal.controllers;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import ru.innopolis.stc31.appeal.model.dto.UserDTO;
 import ru.innopolis.stc31.appeal.services.UsersService;
@@ -18,7 +20,8 @@ import static org.mockito.Mockito.when;
  *
  * @author Sergey Fomin
  */
-@SpringJUnitConfig
+@SpringBootTest
+@ActiveProfiles({"test"})
 class UserControllerTest {
 
     @InjectMocks
