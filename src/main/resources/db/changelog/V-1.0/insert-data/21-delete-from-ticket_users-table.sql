5d6058baffdc5efead09924425c94ea8c9ca0bc1
vadim_sh@bk.ru <vadim_sh@bk.ru>
2021-01-12 11:23:21 +0200

    correct delete ticket_user data

--- src/main/resources/db/changelog/V-1.0/insert-data/21-delete-from-ticket_users-table.sql
+++ src/main/resources/db/changelog/V-1.0/insert-data/21-delete-from-ticket_users-table.sql
@@ -7,7 +7,6 @@ delete from ticket_users where (id_ticket = 1 and id_user = 2) or
                                 (id_ticket = 2 and id_user = 6) or
                                 (id_ticket = 2 and id_user = 8) or
                                 (id_ticket = 3 and id_user = 7) or
-                                (id_ticket = 3 and id_user = 6) or
                                 (id_ticket = 3 and id_user = 3) or
                                 (id_ticket = 3 and id_user = 6) or
                                 (id_ticket = 3 and id_user = 2);
