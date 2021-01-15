fc3d69073fa7c5a737ba9f481848a8663ea98f04
vadim_sh@bk.ru <vadim_sh@bk.ru>
2021-01-15 17:52:48 +0200

    company controller

--- src/main/java/ru/innopolis/stc31/appeal/controllers/CompanyController.java
+++ src/main/java/ru/innopolis/stc31/appeal/controllers/CompanyController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.innopolis.stc31.appeal.converters.CompanyToCompanyDTO;
+import ru.innopolis.stc31.appeal.model.SuccessModel;
 import ru.innopolis.stc31.appeal.model.dto.CompanyDTO;
 import ru.innopolis.stc31.appeal.model.entity.Company;
 import ru.innopolis.stc31.appeal.services.CompanyService;
@@ -41,21 +42,30 @@ public class CompanyController {
     /**
      * Create new company
      *
-     * @param dto Model
+     * @param companyDTO Model
      * @return true if success created
      */
     @PostMapping("/create")
     @ApiOperation("Добавить компанию")
-    public ResponseEntity<CompanyDTO> createCompany(@RequestBody CompanyDTO dto) {
+    public ResponseEntity<CompanyDTO> createCompany(@RequestBody CompanyDTO companyDTO) {
 
-        log.debug("create company method was called with {} ", dto);
+        log.debug("create company method was called with {} ", companyDTO);
 
-        var company = companyService.createCompany(dto);
+        var company = companyService.createCompany(companyDTO);
         if (company == null){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         log.debug("create company method return result {} ", company);
         return new ResponseEntity<>(companyToCompanyDTO.convert(company), HttpStatus.OK);
+    }
+
+    @PostMapping("/delete")
+    @ApiOperation("Удалить компанию")
+    public ResponseEntity<SuccessModel> deleteCompany(@RequestBody CompanyDTO companyDTO) {
+
+        log.debug("delete company method was called with {}", companyDTO);
+
+        boolean isRemoved = companyService.deleteCompany()
     }
 }
