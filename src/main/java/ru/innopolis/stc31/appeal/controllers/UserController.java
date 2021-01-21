ea68e3c60a130f10440c63c0a77bf07230fe16e8
Mikhail Kutcin <makq3345@gmail.com>
2021-01-21 21:34:22 +0500

    Удалены лишние зависимости.

--- src/main/java/ru/innopolis/stc31/appeal/controllers/UserController.java
+++ src/main/java/ru/innopolis/stc31/appeal/controllers/UserController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @AllArgsConstructor
-@RequestMapping("${application.api.uriPrefix}/userTest")
+@RequestMapping("${application.api.uriPrefix}/user")
 public class UserController {
 
     /** Service instance */
--- src/main/java/ru/innopolis/stc31/appeal/repository/TicketRepository.java
+++ src/main/java/ru/innopolis/stc31/appeal/repository/TicketRepository.java
@@ -2,7 +2,6 @@ package ru.innopolis.stc31.appeal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.innopolis.stc31.appeal.model.ModelMock;
 import ru.innopolis.stc31.appeal.model.entity.Ticket;
 
 @Repository
