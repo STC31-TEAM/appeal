e33de7adf9a708e68288ca95079763cfff2468b0
Mikhail Kutcin <makq3345@gmail.com>
2021-01-21 21:19:30 +0500

    - добавлены исправления к таблице users(liquibase); - удалено поле id_map_role в таблице users (liquibase); - добавлено поле id_role в таблице users (liquibase); - добавлены sequence в таблицах users и album_links(liquibase); - в классе CustomAuthencationProvider исправлена обработка определения ролей; - добавлен пакет exceptions с классами, для генерации своих исключений. - Исправлены сущность и модель для Role, а так же сущность User; - реализована возможность добавления пользователя через рест в контроллере UserController, в UsersServiceImpl; - добавлены конверторы UserToUserDTO, UserDTOToUser; - реализованы автотесты для UserController, UsersServiceImpl, UserToUserDTO, UserToUserDTO.

--- src/main/java/ru/innopolis/stc31/appeal/controllers/UserController.java
+++ src/main/java/ru/innopolis/stc31/appeal/controllers/UserController.java
@@ -2,7 +2,12 @@ package ru.innopolis.stc31.appeal.controllers;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.innopolis.stc31.appeal.converters.UserToUserDTO;
+import ru.innopolis.stc31.appeal.exceptions.UsersErrors;
 import ru.innopolis.stc31.appeal.model.dto.UserDTO;
 import ru.innopolis.stc31.appeal.services.UsersService;
 
@@ -11,13 +16,15 @@ import java.util.List;
 /**
  * Controller for manage users
  */
+@Slf4j
 @RestController
 @AllArgsConstructor
-@RequestMapping("${application.api.uriPrefix}/user")
+@RequestMapping("${application.api.uriPrefix}/userTest")
 public class UserController {
 
-    /** Users service instance */
+    /** Service instance */
     private final UsersService usersService;
+    private final UserToUserDTO userToUserDTO;
 
     /**
      * Get list of all users
@@ -34,11 +41,22 @@ public class UserController {
      * Create new user
      *
      * @param dto Model
-     * @return true if success created
+     * @return ResponseEntity<UserDTO>
      */
     @PostMapping("/create")
     @ApiOperation("Добавить пользователя")
-    public boolean createUser(@RequestBody UserDTO dto) {
-        return usersService.createUser(dto);
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO dto) throws UsersErrors {
+
+        log.debug("create user method was called with {} ", dto);
+
+        var user = usersService.createUser(dto);
+
+        if(user == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        log.debug("create user method return result {} ", user);
+
+        return new ResponseEntity<>(userToUserDTO.convert(user), HttpStatus.OK);
     }
 }
---
+++ src/main/java/ru/innopolis/stc31/appeal/services/Roles.java
@@ -0,0 +1,5 @@
+package ru.innopolis.stc31.appeal.services;
+
+public enum Roles {
+    ADMIN, USER, MODERATOR
+}
