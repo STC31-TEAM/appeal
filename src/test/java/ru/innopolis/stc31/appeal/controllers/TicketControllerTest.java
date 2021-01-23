f8997e1c8e78ab9a6dac7fcd492e2fc4152c09f4
EvgenyRusskih <russckih.e@yandex.ru>
2021-01-23 15:14:19 +0400

    TEst TicketControllerTest add

--- src/test/java/ru/innopolis/stc31/appeal/controllers/TicketControllerTest.java
+++ src/test/java/ru/innopolis/stc31/appeal/controllers/TicketControllerTest.java
@@ -3,14 +3,20 @@ package ru.innopolis.stc31.appeal.controllers;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import ru.innopolis.stc31.appeal.converters.TicketDTOToTicket;
 import ru.innopolis.stc31.appeal.model.dto.TicketDTO;
+import ru.innopolis.stc31.appeal.model.entity.Ticket;
 import ru.innopolis.stc31.appeal.services.TicketService;
 import ru.innopolis.stc31.appeal.utils.MockUtils;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 /**
@@ -24,6 +30,11 @@ class TicketControllerTest {
 
     @Mock
     private TicketService service;
+
+    @Spy
+    private TicketDTOToTicket ticketDTOToTicket;
+
+
 
     @Test
     void checkOnOk() {
@@ -41,10 +52,18 @@ class TicketControllerTest {
 
     @Test
     void createTicketWithOk() {
-        TicketDTO ticket = MockUtils.makeTicketDTO();
+        LocalDate dateOpen = LocalDate.of(2021, 1, 22);
+        LocalDate dateClose = LocalDate.of(2021, 1, 23);
 
-        when(service.createTicket(ticket)).thenReturn(true);
+        TicketDTO ticketDTO = new TicketDTO(1,1,1,1,1,1,1,1,
+                "TestTicket1","TestTicketDescription1",dateOpen,dateClose,
+                10,1,(short)0);
+        Ticket ticket=ticketDTOToTicket.convert(ticketDTO);
 
-        assertTrue(controller.createTicket(ticket));
+        when(service.createTicket(ticketDTO)).thenReturn(ticket);
+        ResponseEntity<TicketDTO> responseEntity=controller.createTicket(ticketDTO);
+        assertEquals(responseEntity.getStatusCodeValue(), 200);
+        assertEquals(responseEntity.getBody().getTitles(), ticketDTO.getTitles());
+
     }
 }
