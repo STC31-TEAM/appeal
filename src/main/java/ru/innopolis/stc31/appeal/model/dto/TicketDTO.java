04c4e46019b3b69ac6e7d75c2d28cf5a740024e9
EvgenyRusskih <russckih.e@yandex.ru>
2021-01-23 11:54:53 +0400

    TicketDTO ready

--- src/main/java/ru/innopolis/stc31/appeal/model/dto/TicketDTO.java
+++ src/main/java/ru/innopolis/stc31/appeal/model/dto/TicketDTO.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.time.LocalDate;
+
 /**
  * Ticket data transfer object
  */
@@ -11,4 +13,20 @@ import lombok.experimental.Accessors;
 @NoArgsConstructor
 @Accessors(chain = true)
 public class TicketDTO {
+    private long id;
+    private long idUser;
+    private long idCompany;
+    private long idCountry;
+    private long idCity;
+    private long idStreet;
+    private long idTypesServices;
+    private long idLink;
+    private String titles;
+    private String description;
+    private LocalDate openDate;
+    private LocalDate closeDate;
+    private int countLikes;
+    private int countDisLikes;
+    private short status;
+
 }
