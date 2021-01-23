21b83295016b3afe8245ce689e9d22b6ca61ba8e
EvgenyRusskih <russckih.e@yandex.ru>
2021-01-23 11:56:06 +0400

    TicketDTO fix and ready

--- src/main/java/ru/innopolis/stc31/appeal/model/dto/TicketDTO.java
+++ src/main/java/ru/innopolis/stc31/appeal/model/dto/TicketDTO.java
@@ -1,9 +1,9 @@
 package ru.innopolis.stc31.appeal.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
-
 import java.time.LocalDate;
 
 /**
@@ -11,6 +11,7 @@ import java.time.LocalDate;
  */
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Accessors(chain = true)
 public class TicketDTO {
     private long id;
