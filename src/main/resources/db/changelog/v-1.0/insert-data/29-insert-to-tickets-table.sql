1e2905ade038e0afbed689e69f3b115b1554e418
vadim_sh@bk.ru <vadim_sh@bk.ru>
2021-01-31 22:53:06 +0200

    add company

---
+++ src/main/resources/db/changelog/v-1.0/insert-data/29-insert-to-tickets-table.sql
@@ -0,0 +1,33 @@
+insert into tickets
+values(4,                                                 /* id */
+            1,                                                  /* user */
+            4,                                               /* company */
+            1,                                                  /* country */
+            8,                                                  /* city */
+            12,                                                  /* street */
+            7,                                                  /* service type */
+            4,                                                  /* album link */
+            'Открыть магазин одежды',                /* title */
+            'В северной части города нет ни одного магазина одежды.',     /* description */
+            '2020-12-12',                                       /* open date */
+            '2020-12-28',                                               /* close date */
+            100,                                                  /* count likes */
+            43,                                                  /* count dislikes */
+            2);                                                 /* status */
+
+insert into tickets
+values(5,                                                 /* id */
+            4,                                                  /* user */
+            5,                                               /* company */
+            1,                                                  /* country */
+            9,                                                  /* city */
+            10,                                                  /* street */
+            8,                                                  /* service type */
+            5,                                                  /* album link */
+            'Построить автомойку',                /* title */
+            'На весь город одна автомойка. Хотим ещё.',     /* description */
+            '2020-12-12',                                       /* open date */
+            '2020-12-28',                                               /* close date */
+            100,                                                  /* count likes */
+            43,                                                  /* count dislikes */
+            2);                                                 /* status */
