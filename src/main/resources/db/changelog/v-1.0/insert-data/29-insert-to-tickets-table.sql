c3ddc3b90dbd3c72545b49f14e965e0b9452352c
vadim_sh@bk.ru <vadim_sh@bk.ru>
2021-01-31 23:05:06 +0200

    ready to merge

--- src/main/resources/db/changelog/v-1.0/insert-data/29-insert-to-tickets-table.sql
+++ src/main/resources/db/changelog/v-1.0/insert-data/29-insert-to-tickets-table.sql
@@ -26,8 +26,94 @@ values(5,                                                 /* id */
             5,                                                  /* album link */
             'Построить автомойку',                /* title */
             'На весь город одна автомойка. Хотим ещё.',     /* description */
-            '2020-12-12',                                       /* open date */
-            '2020-12-28',                                               /* close date */
-            100,                                                  /* count likes */
-            43,                                                  /* count dislikes */
+            '2020-12-24',                                       /* open date */
+            '2020-12-31',                                               /* close date */
+            37,                                                  /* count likes */
+            12,                                                  /* count dislikes */
             2);                                                 /* status */
+
+insert into tickets
+values(6,                                                 /* id */
+            5,                                                  /* user */
+            NULL,                                               /* company */
+            1,                                                  /* country */
+            10,                                                  /* city */
+            14,                                                  /* street */
+            10,                                                  /* service type */
+            6,                                                  /* album link */
+            'Ремонт моста',                /* title */
+            'Под мост начали проваливаться машины. Не хорошо.',     /* description */
+            '2021-01-02',                                       /* open date */
+            NULL,                                               /* close date */
+            55,                                                  /* count likes */
+            8,                                                  /* count dislikes */
+            1);                                                 /* status */
+
+insert into tickets
+values(7,                                                 /* id */
+            2,                                                  /* user */
+            NULL,                                               /* company */
+            1,                                                  /* country */
+            11,                                                  /* city */
+            15,                                                  /* street */
+            11,                                                  /* service type */
+            7,                                                  /* album link */
+            'Открыть подпольное казино',                /* title */
+            'Не помешает.',     /* description */
+            '2021-01-10',                                       /* open date */
+            NULL,                                               /* close date */
+            25,                                                  /* count likes */
+            54,                                                  /* count dislikes */
+            1);                                                 /* status */
+
+insert into tickets
+values(8,                                                 /* id */
+            6,                                                  /* user */
+            NULL,                                               /* company */
+            1,                                                  /* country */
+            12,                                                  /* city */
+            4,                                                  /* street */
+            10,                                                  /* service type */
+            8,                                                  /* album link */
+            'Почистить озеро',                /* title */
+            'Не помешает.',     /* description */
+            '2021-01-10',                                       /* open date */
+            NULL,                                               /* close date */
+            63,                                                  /* count likes */
+            31,                                                  /* count dislikes */
+            1);                                                 /* status */
+
+
+insert into tickets
+values(9,                                                 /* id */
+            7,                                                  /* user */
+            NULL,                                               /* company */
+            1,                                                  /* country */
+            13,                                                  /* city */
+            5,                                                  /* street */
+            10,                                                  /* service type */
+            9,                                                  /* album link */
+            'Вырубить лес',                /* title */
+            'Бегают волки.',     /* description */
+            '2021-01-15',                                       /* open date */
+            NULL,                                               /* close date */
+            32,                                                  /* count likes */
+            75,                                                  /* count dislikes */
+            1);                                                 /* status */
+
+insert into tickets
+values(10,                                                 /* id */
+            7,                                                  /* user */
+            NULL,                                               /* company */
+            1,                                                  /* country */
+            13,                                                  /* city */
+            7,                                                  /* street */
+            12,                                                  /* service type */
+            10,                                                  /* album link */
+            'Построить пиццерию',                /* title */
+            'В селе не где поесть.',     /* description */
+            '2021-01-21',                                       /* open date */
+            NULL,                                               /* close date */
+            76,                                                  /* count likes */
+            27,                                                  /* count dislikes */
+            1);                                                 /* status */
