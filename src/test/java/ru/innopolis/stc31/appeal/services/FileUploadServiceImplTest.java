7bdbac5b0b908d39416bd08c28d2c9cbf0ec980c
SFomin <sergei.fomin@list.ru>
2021-01-31 19:25:38 +0500

    added tests

--- src/test/java/ru/innopolis/stc31/appeal/services/FileUploadServiceImplTest.java
+++ src/test/java/ru/innopolis/stc31/appeal/services/FileUploadServiceImplTest.java
@@ -1,12 +1,22 @@
 package ru.innopolis.stc31.appeal.services;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Uploader;
+import com.cloudinary.utils.ObjectUtils;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringJUnitConfig
@@ -15,15 +25,30 @@ public class FileUploadServiceImplTest {
     @InjectMocks
     private FileUploadService fileUploadService;
 
+    @Spy
+    private Cloudinary cloudinary;
+
+    @Mock
+    private Uploader uploader;
+
     @Mock
     private MultipartFile multipartFile;
 
     @Test
-    void checkOnOk() {
-        when(multipartFile.isEmpty()).thenReturn(true);
-        when(multipartFile.getOriginalFilename()).thenReturn("example.png");
+    void checkOnOk() throws IOException {
+        String inputFileName = "example.png";
+        String uploadedFileName = "get_image_url.png";
+
+        when(!multipartFile.isEmpty()).thenReturn(false);
+        when(multipartFile.getOriginalFilename()).thenReturn(inputFileName);
+        when(cloudinary.uploader()).thenReturn(uploader);
+
+        Map<String, String> resultUpload = new HashMap<>();
+        resultUpload.put("secure_url", uploadedFileName);
+
+        when(uploader.upload(new File(inputFileName), ObjectUtils.emptyMap())).thenReturn(resultUpload);
 
         assertDoesNotThrow(() -> fileUploadService.upload(null));
-        assertDoesNotThrow(() -> fileUploadService.upload(multipartFile));
+        assertEquals(uploadedFileName, fileUploadService.upload(multipartFile));
     }
 }
